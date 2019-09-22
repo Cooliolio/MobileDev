a0df70c1488e194fac0afe4fb0b9e11c336a202a
Furkan <furkanpinar9@hotmail.com>
2019-09-22 23:23:22 +0200

    -ViewFragments HomeFragment: Auto-Generated -Drawable Created multiple buttons: Home,Profile,Add,Notifications -Layout Created fragment_home and post_list_item --> To be continued -Menu Created bottom_main_nav --> Can be redesigned in the future -Values Added strings for the navbar buttons
    ---> Pair Programmed with Yasin Yavuz

--- app/src/main/java/com/MobDev/die_vers/ViewActivities/MainActivity.java
+++ app/src/main/java/com/MobDev/die_vers/ViewActivities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.MobDev.die_vers;
+package com.MobDev.die_vers.ViewActivities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.MobDev.die_vers.R;
 
 public class MainActivity extends AppCompatActivity {
 
